1bb6a099cd5a17ecf160630a218ffe7c5a167847
ManishKumarcsCognizant <Manish.Kumarc3506@cognizant.com>
2026-06-08 16:32:28 +0530

    Update API URL for production environment

--- HealthcareManagement-Frontend/src/environments/environment.prod.ts
+++ HealthcareManagement-Frontend/src/environments/environment.prod.ts
@@ -1,10 +1,10 @@
 export const environment = {
   production: true,
-  apiURL: 'http://localhost:8081'
+  apiURL: 'https://healthcaremanagementsystem-mediconnect.onrender.com'
 };
 
 // Production API URL - set to backend port used locally or update during deployment
 export const prodEnvironment = {
   production: true,
-  apiURL: 'http://localhost:8081'
+  apiURL: 'https://healthcaremanagementsystem-mediconnect.onrender.com'
 };
